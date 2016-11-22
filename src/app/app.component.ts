e1fc8d5b4775e64eaab0b6763e73cabac322010a
whj768702 <whj768702@gmail.com>
2016-11-22 22:05:32 +0800

    practise Routing. it has a problem "moduleId should be string".

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
-import {Hero} from "./hero";
-import {HeroService} from "./hero.service";
-
-// import '../../public/css/styles.css';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    providers: [HeroService],
-    template:  `
-        <h2>My Heroes</h2>
-        <ul class="heroes">
-            <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
-                <span class="badge">{{hero.id}}</span>{{hero.name}}
-            </li> 
-        </ul>
-        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a routerLinke="/dashboard">Dashboard</a>
+            <a routerLinke="/heroes">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
     `,
-    styleUrls: ['./app.component.css']
+    styleUrls: ['app.component.css']
 })
 
-export class AppComponent implements OnInit{
-    heroes: Hero[];
-    selectedHero: Hero;
-
-    constructor(private heroService: HeroService){
-    }
-
-    onSelect(hero: Hero): void{
-        this.selectedHero = hero;
-    }
-    getHeroes(): void{
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    }
-    ngOnInit(): void{
-        this.getHeroes();
-    }
+export class AppComponent{
+    title: string = 'Tour of Heroes';
 }
-
-
