9ac022895e65365bde655686fa84f4ea53ff0958
whj768702 <whj768702@gmail.com>
2016-11-22 22:05:32 +0800

    practise Routing. it has a problem "moduleId should be string".

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Hero} from "./hero";
+import {HeroService} from "./hero.service";
+import {Router} from "@angular/router";
+
+// import '../../public/css/styles.css';
+
+@Component({
+    // moduleId: module.id,
+    selector: 'my-heroes',
+    providers: [HeroService],
+    templateUrl: './heroes.component.html'
+    // ,
+    // styleUrls: ['heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit{
+    heroes: Hero[];
+    selectedHero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private router: Router){
+    }
+
+    onSelect(hero: Hero): void{
+        this.selectedHero = hero;
+    }
+    getHeroes(): void{
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+    ngOnInit(): void{
+        this.getHeroes();
+    }
+    gotoDetail(): void{
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
+
+
