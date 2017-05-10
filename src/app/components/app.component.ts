095d7e063517318f3067958ea92ee4ba0ed7424f
whj768702 <whj768702@gmail.com>
2017-05-10 08:40:16 +0800

    通过import加载公共样式.

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import '../../../node_modules/jquery/dist/jquery';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../../public/css/styles.css';
 
 @Component({
     selector: 'sg-my-app',
