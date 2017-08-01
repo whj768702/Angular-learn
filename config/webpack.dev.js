26ebcf5a9d0e353c7ef9529fefb82383a07fbb9c
whj768702 <whj768702@gmail.com>
2017-08-01 16:13:58 +0800

    增加代码组成分析webpack-bundle-analyzer。升级rxjs到5.4.2，解决typescript2.4.x版本中rxjs报错问题。

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,7 @@ var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = webpackMerge(commonConfig, {
     devtool: 'cheap-module-eval-source-map',
@@ -15,7 +16,7 @@ module.exports = webpackMerge(commonConfig, {
 
     plugins: [
         new ExtractTextPlugin('[name].css'),
-
+        new BundleAnalyzerPlugin()
     ],
 
     devServer: {
