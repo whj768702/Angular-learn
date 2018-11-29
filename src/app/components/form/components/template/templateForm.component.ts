0e677d1eb5dea32dcbafd086abfc6f11cbf5954a
whj768702 <whj768702@gmail.com>
2018-11-29 08:30:48 +0800

    模板驱动表单。

--- src/app/components/form/components/template/templateForm.component.ts
+++ src/app/components/form/components/template/templateForm.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Person} from './person';
 
 @Component({
@@ -7,12 +7,15 @@ import {Person} from './person';
   styleUrls: ['./templateForm.less']
 })
 
-export class TemplateFormComponent {
+export class TemplateFormComponent implements OnInit {
   courses = ['math', 'sport', 'music'];
   person = new Person(1, 'w', 18, this.courses[0]);
   submitted = false;
 
   constructor() {
+  }
+
+  ngOnInit() {
   }
 
   onSubmit() {
